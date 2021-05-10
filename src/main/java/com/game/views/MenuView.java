36ce81d186d715cc0fa88b7d4a48c0a004b137aa
Dominik <dominik_pruetting@outlook.de>
2021-05-10 19:46:27 +0200

    added Hinweis auf exit

--- src/main/java/com/game/views/MenuView.java
+++ src/main/java/com/game/views/MenuView.java
@@ -10,5 +10,8 @@ public class MenuView extends View{
     @Override
     public void render(Graphics2D g, int frame) {
         g.drawImage(AssetPool.getTexture("res/images/Info_Jump'n'Run.png").getImg(), 0, 0, Settings.WIDTH, Settings.HEIGHT, null);
+        g.setColor(Color.lightGray);
+        g.setFont(new Font("Hack", Font.PLAIN, 24));
+        g.drawString("Exit with ESC (Voruebergehend)", Settings.WIDTH - 500 , Settings.HEIGHT - 300);
     }
 }
